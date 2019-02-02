30c7e291d9db077bc9e3db4a0bd71c1a51b953ae
James Fennell <jamespfennell@gmail.com>
2019-02-02 15:44:14 -0500

    Fix manifest.in

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='transiter_nycsubway',
     version='0.1dev',
     packages=find_packages(),
-    include_package_data = True,
+    include_package_data=True,
     install_requires = [
         'python-dateutil',
 #        'transiter==0.1dev'
