7140f51c9adce94e21561c46b62d24155b4fc23c
james <jamespfennell@gmail.com>
2020-05-30 18:09:19 -0400

    Add more tests

--- transiter_ny_mta/tests/test_subwaytripsparser.py
+++ transiter_ny_mta/tests/test_subwaytripsparser.py
@@ -26,7 +26,6 @@ TIME_2 = datetime.datetime.utcfromtimestamp(2000).replace(tzinfo=pytz.UTC)
     ],
 )
 def test_direction(existing_direction_id, mta_direction, expected_direction_id):
-
     trip = gtfs.TripDescriptor(trip_id=TRIP_ID)
     if existing_direction_id is not None:
         expected_direction_id = existing_direction_id
@@ -50,21 +49,43 @@ def test_direction(existing_direction_id, mta_direction, expected_direction_id):
     assert [expected_trip] == actual_trips
 
 
-def test_create_vehicle():
-    pass
+@pytest.mark.parametrize(
+    "entity_key,entity_type",
+    [["trip_update", gtfs.TripUpdate], ["vehicle", gtfs.VehiclePosition]],
+)
+@pytest.mark.parametrize(
+    "is_assigned,expect_vehicle", [[True, True], [False, False], [None, False]]
+)
+@pytest.mark.parametrize(
+    "train_id,expected_vehicle_id",
+    [[VEHICLE_ID, VEHICLE_ID], [None, "vehicle_" + TRIP_ID]],
+)
+def test_create_vehicle(
+    entity_key, entity_type, is_assigned, expect_vehicle, train_id, expected_vehicle_id
+):
+    trip = gtfs.TripDescriptor(trip_id=TRIP_ID)
+    mta_ext = trip.Extensions[trip._extensions_by_number[gtfs.MTA_EXTENSION_ID]]
+    if train_id is not None:
+        mta_ext.train_id = train_id
+    if is_assigned is not None:
+        mta_ext.is_assigned = is_assigned
 
+    entity_key_to_entity = {entity_key: entity_type(trip=trip)}
+    message = gtfs.FeedMessage(
+        header=gtfs.FeedHeader(gtfs_realtime_version="2.0"),
+        entity=[gtfs.FeedEntity(id="1", **entity_key_to_entity)],
+    )
 
-def test_create_vehicle__use_trip_id():
-    pass
+    expected_vehicle = parse.Vehicle(id=expected_vehicle_id, trip_id=TRIP_ID)
 
+    parser = SubwayTripsParser()
+    parser.load_content(message.SerializeToString())
+    actual_vehicles = list(parser.get_vehicles())
 
-def test_create_vehicle__skip_unassigned_trip():
-    # Test both explictely unassigned and missing
-    pass
-
-
-def test_create_vehicle__do_not_override():
-    pass
+    if expect_vehicle:
+        assert [expected_vehicle] == actual_vehicles
+    else:
+        assert [] == actual_vehicles
 
 
 def test_track():
