5e4468f8a588537ca816e33ab2c00fb8bb6846eb
james <jamespfennell@gmail.com>
2020-05-31 09:51:09 -0400

    Add stop direction inversion in Bushwick

--- transiter_ny_mta/tests/test_subwaytripsparser.py
+++ transiter_ny_mta/tests/test_subwaytripsparser.py
@@ -10,6 +10,9 @@ from transiter_ny_mta.proto import subwaytrips_pb2 as gtfs
 ALERT_ID = "alert_id"
 TRIP_ID = "trip_id"
 VEHICLE_ID = "vehicle_id"
+STOP_ID = "stop_id"
+TRACK_1_ID = "track_1_id"
+TRACK_2_ID = "track_2_id"
 TIME_1 = datetime.datetime.utcfromtimestamp(1000).replace(tzinfo=pytz.UTC)
 TIME_2 = datetime.datetime.utcfromtimestamp(2000).replace(tzinfo=pytz.UTC)
 
@@ -88,6 +91,95 @@ def test_create_vehicle(
         assert [] == actual_vehicles
 
 
-def test_track():
-    # Test 3 cases
-    pass
+@pytest.mark.parametrize(
+    "scheduled_track,actual_track,expected_track",
+    [
+        [None, None, None],
+        [None, TRACK_1_ID, TRACK_1_ID],
+        [TRACK_1_ID, None, TRACK_1_ID],
+        [TRACK_1_ID, TRACK_2_ID, TRACK_2_ID],
+    ],
+)
+def test_track(scheduled_track, actual_track, expected_track):
+    stop_time = gtfs.TripUpdate.StopTimeUpdate(stop_id=STOP_ID)
+    mta_ext = stop_time.Extensions[
+        stop_time._extensions_by_number[gtfs.MTA_EXTENSION_ID]
+    ]
+    if scheduled_track is not None:
+        mta_ext.scheduled_track = scheduled_track
+    if actual_track is not None:
+        mta_ext.actual_track = actual_track
+
+    message = gtfs.FeedMessage(
+        header=gtfs.FeedHeader(gtfs_realtime_version="2.0"),
+        entity=[
+            gtfs.FeedEntity(
+                id="1",
+                trip_update=gtfs.TripUpdate(
+                    trip=gtfs.TripDescriptor(trip_id=TRIP_ID),
+                    stop_time_update=[stop_time],
+                ),
+            )
+        ],
+    )
+
+    expected_trip = parse.Trip(
+        id=TRIP_ID,
+        direction_id=False,
+        stop_times=[parse.TripStopTime(stop_id=STOP_ID, track=expected_track)],
+    )
+
+    parser = SubwayTripsParser()
+    parser.load_content(message.SerializeToString())
+    actual_trips = list(parser.get_trips())
+
+    assert [expected_trip] == actual_trips
+
+
+STOP_IDS_TO_INVERT = {"M11", "M12", "M13", "M14", "M16", "M18"}
+
+
+@pytest.mark.parametrize(
+    "route_id,stop_id,expected_stop_id",
+    [
+        ["M", f"{stop_id}{old_direction}", f"{stop_id}{new_direction}"]
+        for stop_id in STOP_IDS_TO_INVERT
+        for old_direction, new_direction in [["N", "S"], ["S", "N"]]
+    ]
+    + [
+        ["J", f"{stop_id}{old_direction}", f"{stop_id}{old_direction}"]
+        for stop_id in STOP_IDS_TO_INVERT
+        for old_direction in ["S", "N"]
+    ]
+    + [
+        ["M", f"M10{old_direction}", f"M10{old_direction}"]
+        for old_direction in ["S", "N"]
+    ],
+)
+def test_invert_stop_id_direction_in_bushwick(route_id, stop_id, expected_stop_id):
+
+    message = gtfs.FeedMessage(
+        header=gtfs.FeedHeader(gtfs_realtime_version="2.0"),
+        entity=[
+            gtfs.FeedEntity(
+                id="1",
+                trip_update=gtfs.TripUpdate(
+                    trip=gtfs.TripDescriptor(trip_id=TRIP_ID, route_id=route_id),
+                    stop_time_update=[gtfs.TripUpdate.StopTimeUpdate(stop_id=stop_id)],
+                ),
+            )
+        ],
+    )
+
+    expected_trip = parse.Trip(
+        id=TRIP_ID,
+        route_id=route_id,
+        direction_id=False,
+        stop_times=[parse.TripStopTime(stop_id=expected_stop_id)],
+    )
+
+    parser = SubwayTripsParser()
+    parser.load_content(message.SerializeToString())
+    actual_trips = list(parser.get_trips())
+
+    assert [expected_trip] == actual_trips
--- transiter_ny_mta/transiter_ny_mta/subwaytripsparser.py
+++ transiter_ny_mta/transiter_ny_mta/subwaytripsparser.py
@@ -1,18 +1,24 @@
 import enum
 import sys
 
-from transiter.parse import GtfsRealtimeParser
+from transiter import parse
 from transiter.parse.gtfsrealtime import TRANSITER_EXTENSION_ID
 from transiter_ny_mta.proto import subwaytrips_pb2 as gtfs_rt_pb2
+import typing
 
 
-class SubwayTripsParser(GtfsRealtimeParser):
+class SubwayTripsParser(parse.GtfsRealtimeParser):
 
     GTFS_REALTIME_PB2_MODULE = gtfs_rt_pb2
 
     @staticmethod
     def post_process_feed_message(feed_message):
         _move_data_between_extensions(feed_message)
+
+    def get_trips(self) -> typing.Iterable[parse.Trip]:
+        for trip in super().get_trips():
+            _invert_m_train_direction_in_bushwick(trip)
+            yield trip
 
 
 class _MtaDirection(enum.Enum):
@@ -70,10 +76,31 @@ def _move_data_between_extensions(feed_message):
             else:
                 track = None
 
+            if track is None:
+                continue
             transiter_ext = stop_time.Extensions[
                 stop_time._extensions_by_number[TRANSITER_EXTENSION_ID]
             ]
             transiter_ext.track = track
+
+
+def _invert_m_train_direction_in_bushwick(trip: parse.Trip):
+    route_id = trip.route_id
+    if route_id != "M":
+        return True
+    flipper = {"N": "S", "S": "N"}
+    for stop_time in trip.stop_times:
+        stop_id = stop_time.stop_id
+        if stop_id[:3] not in {"M11", "M12", "M13", "M14", "M16", "M18"}:
+            continue
+        stop_time.stop_id = stop_id[:3] + flipper[stop_id[3]]
+
+
+# TODO: fix next stop in vehicle position
+# TODO: all of the trip cleaners
+# - def fix_route_ids(trip):
+# - def invert_m_train_direction_in_bushwick
+# - delete old scheduled trips?
 
 
 def _create_vehicle_id_from_trip_desc(mta_ext, trip_id):
